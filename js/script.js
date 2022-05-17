dedfc3a2206fca05b2e9a17e00867ef056ca1b51
Andrew <dd@rh-s.com>
2022-05-17 12:23:45 +0300

    initialized full page js

--- js/script.js
+++ js/script.js
@@ -3,4 +3,11 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   
+  $(document).ready(function () {
+    $('#fullpage').fullpage({
+      sectionsColor: ['#FFD200', '#FFDF48'],
+      scrollingSpeed: 1000
+    });
+  });
+
 })
