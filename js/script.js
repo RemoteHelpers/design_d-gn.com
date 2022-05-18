6730e97e08faf9da9197c870a601c4ab1727bf64
Andrew <dd@rh-s.com>
2022-05-18 17:42:29 +0300

    add tabs, change 3-4 screen

--- js/script.js
+++ js/script.js
@@ -2,12 +2,59 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-  
+
   $(document).ready(function () {
     $('#fullpage').fullpage({
-      sectionsColor: ['#FFD200', '#FFDF48'],
-      scrollingSpeed: 1000
+      sectionsColor: ['#FFD200', '#FFDF48', '#FFDF48', '#FFDF48', '#FFDF48', '#FFDF48'],
+      scrollingSpeed: 500
     });
   });
+
+
+  const list = document.querySelector('.portfolio_btns'),
+    cards = document.querySelectorAll('.card'),
+    listItems = document.querySelectorAll('.filter_btn');
+
+
+  function filter() {
+    list.addEventListener('click', event => {
+      const targetID = event.target.dataset.id;
+      const target = event.target;
+      console.log(targetID);
+
+      if (target.classList.contains('filter_btn')) {
+        listItems.forEach(listItem => listItem.classList.remove('filter_btn_active'))
+        target.classList.add('filter_btn_active');
+      }
+
+
+      switch (targetID) {
+        case 'branding':
+          getItems(targetID)
+          break;
+        case 'graphic':
+          getItems(targetID)
+          break;
+        case 'web':
+          getItems(targetID)
+          break;
+      }
+    })
+  }
+  filter()
+
+  function getItems(className) {
+    cards.forEach(card => {
+      if (card.classList.contains(className)) {
+        card.style.display = 'block'
+      } else {
+        card.style.display = 'none'
+      }
+    })
+  }
+
+
+
+
 
 })
